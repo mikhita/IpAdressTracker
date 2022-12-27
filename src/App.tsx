3a4f5ba0c53e226610947015ce7a307283ae3c7b
mikhita <mikheilispiriani@yahoo.com>
2022-12-28 00:42:27 +0400

    adding backroung image and style it

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import bgImage from "./assets/pattern-bg.png"
+// import bgImage from "./assets/pattern-bg.png"
 
 function App() {
   return (
-    <div className="flex flex-col justify-center	items-center	h-full">
-      <img className='w-full h-1/4' src={bgImage} alt="" />
+    <div className="flex flex-col justify-center	items-center w-full	h-full bg-no-repeat  bg-top
+		bg-[url('./assets/pattern-bg.png')] bg-[length:100%_30%]
+		">
+      
+
     </div>
   );
 }
