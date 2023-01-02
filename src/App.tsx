7f8db2cadfde4161790c7547a8e3545aed8f6a15
mikhita <mikheilispiriani@yahoo.com>
2023-01-02 19:34:09 +0400

    adding ipadress state to the details and input components from app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,31 @@ import React from 'react';
 import Details from './components/Details';
 import Header from './components/Header';
 import Input from './components/Input';
+import { useState } from 'react';
 
 
 
 
 function App() {
+const [ipAdress, setIpAdress] = useState("");
+
+interface FormDataType {ipAdress:string};
+const responseBody: FormDataType = {ipAdress: ""};
+
+const onSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+  event.preventDefault();
+  responseBody.ipAdress = ipAdress
+}
+const inputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+  setIpAdress(event.target.value)
+}
 
   return (
     <div className="flex flex-col justify-center	items-center w-full	h-full bg-no-repeat  bg-top
 		bg-[url('./assets/pattern-bg.png')] bg-[length:100%_60%]">
       <Header/>
-      <Input />
-      <Details/>
+      <Input onSubmitHandler={onSubmitHandler} inputChangeHandler={inputChangeHandler} />
+      <Details ipAdress={ipAdress}/>
     </div>
   );
 }
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
-import Arrow from "../assets/icon-arrow.svg"
+
 
 interface Props {
-    stringProp: string;
-    numberProp: number;
-    
+    inputChangeHandler: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    onSubmitHandler: (e:React.FormEvent<HTMLFormElement>) => void;
+
 }
 
 function Input(props: Props) {
    
   return (
     <div className='flex w-80 justify-center align-middle xl:shrink min-w-[40%] mt-7'>
-        <input  placeholder='Search for any IP address' className='px-6 py-4 font-rubik text-sm rounded-l-2xl xl:text-xl shrink w-11/12 border-none focus:border-none outline-0'/>
-        <div className='rounded-r-2xl bg-black w-14 flex justify-center align-middle'><img  src={Arrow} alt="" className='w-3 h-3 self-center'/></div>
+        <form className='flex w-80 justify-center align-middle xl:shrink min-w-[40%] mt-7' onSubmit={props.onSubmitHandler}>
+            <input placeholder='Search for any IP address' className='px-6 py-4 font-rubik text-sm rounded-l-2xl xl:text-xl shrink w-11/12 border-none focus:border-none outline-0'  onChange={props.inputChangeHandler} type="text"/>
+            <input type="submit" className='rounded-r-2xl bg-black w-14 flex justify-center align-middle text-cyan-50 text-3xl' value=">"/>
+        </form>
     </div>
   )
 }
