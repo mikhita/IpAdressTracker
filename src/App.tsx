888066a7c0e924dc725b9351d4d5a84798c3d19f
mikhita <mikheilispiriani@yahoo.com>
2023-01-02 13:12:49 +0400

    ready for adding dynamic routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,18 @@ import React from 'react';
 import Details from './components/Details';
 import Header from './components/Header';
 import Input from './components/Input';
-import {
-  MapContainer,
-  TileLayer,
-  useMap,
-} from 'https://cdn.esm.sh/react-leaflet'
+
+
+
 
 function App() {
+
   return (
     <div className="flex flex-col justify-center	items-center w-full	h-full bg-no-repeat  bg-top
 		bg-[url('./assets/pattern-bg.png')] bg-[length:100%_60%]">
       <Header/>
-      <Input/>
+      <Input />
       <Details/>
-
     </div>
   );
 }
